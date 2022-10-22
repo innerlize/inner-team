496510cf8b7d494ec9c8bd8d9f893d83352578ed
charlyuni <charlessactis@gmail.com>
2022-10-22 20:53:02 -0300

    charly3

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -4,17 +4,28 @@ import About from "../components/About/About"
 import Servicios from "../components/Servicios/Servicios"
 import Exportacion from "../components/Exportacion/Exportacion"
 import WhastappFloat from "../components/Whatsapp/WhastappFloat"
+import FAQSection from "../FAQ-Section/FAQ-Section"
+import TestimonialsSection from "../Testimonials-Section/Testimonials-Section"
+import ClientsSection from "../Clients-Section/Clients-Section"
+import ContactSection from "../Contact-Section/Contact-Section"
+import FooterSection from "../Footer-Section/Footer-Section"
 
 
 const Landing = () => {
   return (
     <>
-    <NavigationDefault />
-    <Headers />
-    <About />
-    <Servicios />
-    <Exportacion/>
-    <WhastappFloat />
+      <NavigationDefault />
+      <Headers />
+      <About />
+      <Servicios />
+      <Exportacion />
+      <WhastappFloat />
+      <FAQSection />
+      <TestimonialsSection />
+      <ClientsSection />
+      <ContactSection />
+      <FooterSection />
+
     </>
   )
 }
