1f3ee4954a7f959c451797003dc9a1c6b8c00304
charlyuni <charlessactis@gmail.com>
2022-10-22 21:16:55 -0300

    charly7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import './App.css';
-import Router from './Router/Router.js';
 
+import './App.css';
+import {Route, Routes , BrowserRouter} from 'react-router-dom';
+import  Landing from './components/pages/Landing';
+import BlogSection from './components/Blog-Section/Blog-Section';
+import BlogDetails1 from './components/Blog-Section/Blog-Details/Blog-Details_1';
+import BlogDetails2 from './components/Blog-Section/Blog-Details/Blog-Details_2';
+import BlogDetails3 from './components/Blog-Section/Blog-Details/Blog-Details_3';
+import BlogDetails4 from './components/Blog-Section/Blog-Details/Blog-Details_4';
+import BlogDetails5 from './components/Blog-Section/Blog-Details/Blog-Details_5';
+import NavigationDefault from './components/Nav/NavigationDefault';
+import FooterSection from './components/Footer-Section/Footer-Section';
 
 function App() {
   return (
-      <Router />
+    <BrowserRouter>
+      <NavigationDefault />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path='/blog' element={ <BlogSection /> } />
+				<Route path='/blog/details1' element={ <BlogDetails1 /> } />
+				<Route path='/blog/details2' element={ <BlogDetails2 /> } />
+				<Route path='/blog/details3' element={ <BlogDetails3 /> } />
+				<Route path='/blog/details4' element={ <BlogDetails4 /> } />
+				<Route path='/blog/details5' element={ <BlogDetails5 /> } />
+      </Routes>
+      <FooterSection />
+    </BrowserRouter>
   );
 }
 
-
 export default App;
+
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,20 +1,18 @@
-import NavigationDefault from "../components/Nav/NavigationDefault"
-import Headers from "../components/Header/Header"
-import About from "../components/About/About"
-import Servicios from "../components/Servicios/Servicios"
-import Exportacion from "../components/Exportacion/Exportacion"
-import WhastappFloat from "../components/Whatsapp/WhastappFloat"
+import Headers from "../Header/Header";
+import About from "../About/About"
+import Servicios from "../Servicios/Servicios"
+import Exportacion from "../Exportacion/Exportacion"
+import WhastappFloat from "../Whatsapp/WhastappFloat"
 import FAQSection from "../FAQ-Section/FAQ-Section"
 import TestimonialsSection from "../Testimonials-Section/Testimonials-Section"
 import ClientsSection from "../Clients-Section/Clients-Section"
 import ContactSection from "../Contact-Section/Contact-Section"
-import FooterSection from "../Footer-Section/Footer-Section"
 
 
 const Landing = () => {
   return (
     <>
-      <NavigationDefault />
+
       <Headers />
       <About />
       <Servicios />
@@ -24,7 +22,6 @@ const Landing = () => {
       <TestimonialsSection />
       <ClientsSection />
       <ContactSection />
-      <FooterSection />
 
     </>
   )
