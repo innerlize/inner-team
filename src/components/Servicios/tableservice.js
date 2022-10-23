9fbf06ae6a0d32cb50711935ecbebe552114551d
charlyuni <charlessactis@gmail.com>
2022-10-22 22:30:41 -0300

    54

--- src/components/Servicios/tableservice.js
+++ src/components/Servicios/tableservice.js
@@ -3,14 +3,14 @@ const servicies = [
     {
         id: 1,
         title: 'Administración',
-        image: servicio1,
+        image: 'bg-servicio1',
         description: 'Administración Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
         active: false
     },
     {
         id: 2,
         title: 'Consultoría',
-        image: './servicio2.png',
+        image: 'bg-servicio2',
         description: 'Consultoría Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
         active: false
     },
